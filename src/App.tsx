4d1b1a9dda8e74750a6cbe5170e7c055ad9dc020
goncalo.regalado <gf.regalado@gmail.com>
2022-02-12 07:30:30 +0000

    Copy project boilerplate and update node version to 16

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import logo from './logo.svg'
 import './App.css'
 
 function App(): JSX.Element {
+    const wave = (): void => {}
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className="mainContainer">
+            <div className="dataContainer">
+                <div className="header">👋 Hey there!</div>
+
+                <div className="bio">
+                    I am Gonçalo and I worked as an artis for a while so
+                    that&apos;s pretty cool right? Connect your Ethereum wallet
+                    and wave at me!
+                </div>
+                <button type="button" className="waveButton" onClick={wave}>
+                    Wave at Me
+                </button>
+            </div>
         </div>
     )
 }
